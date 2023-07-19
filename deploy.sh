4377053c109cd5971ac84bd12fb4675aa8696255
🐻 <hongphat.js@gmail.com>
2023-07-19 11:53:28 +0700

    update sync config & purge tailwindcss

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 rm -rf www
 mkdir www
 curl https://zalo-miniapp.github.io/next-mini-app/ > www/index.html
-zmp sync www/index.html -M "#__next"
+zmp sync-config www/index.html -r "#__next"
 zmp deploy -e -m "$(git log -1 --pretty=%B)"
